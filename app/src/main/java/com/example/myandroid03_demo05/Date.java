73e5b97d204204ba9814d1c387d34a722a3fe074
吴肖光 <891211031@qq.com>
2017-10-18 21:33:47 +0800

    new Demo5

---
+++ app/src/main/java/com/example/myandroid03_demo05/Date.java
@@ -0,0 +1,30 @@
+package com.example.myandroid03_demo05;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 吴肖光 on 2017/10/18.
+ */
+public class Date extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
